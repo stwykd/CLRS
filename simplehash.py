3c4d1928e229f4026b0fcbb9677155ab6a170ebc
stwykd <5012027+stwykd@users.noreply.github.com>
2020-01-09 13:11:53 +0100

    implement simple hash

---
+++ simplehash.py
@@ -0,0 +1,15 @@
+class h_table:
+    def __init__(self):
+        self.table=[[]]*20
+
+    def insert(self, index, val):
+        k=h_func(index)
+        self.table[k].append((table,val))
+
+    def h_func(index):
+        return index%20
+
+    def retrieve(input):
+        for k in table[input%20]:
+            if k[0]==input
+            return k[1]
