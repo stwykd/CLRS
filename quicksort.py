f4d5f58ce85b574749ac3b928d33b4a10194fd80
stwykd <5012027+stwykd@users.noreply.github.com>
2020-04-29 21:09:49 +0100

    simplify quick sort

--- quicksort.py
+++ quicksort.py
@@ -1,30 +1,10 @@
+import random
+
+
 def quick_sort(array):
-    quick_sort(array, 0, len(array) - 1)
-
-
-def quick_sort(array, left, right):
-    if left >= right:
-        return
-    pivot = array[(left + right) / 2]
-    index = partition(array, left, right, pivot)
-    quick_sort(array, left, index - 1)
-    quick_sort(array, index, right)
-
-
-def swap(array, left, right):
-    tmp = array[left]
-    array[left] = array[right]
-    array[right] = tmp
-
-
-def partition(array, left, right, pivot):
-    while left <= right:
-        while array[left] < pivot:
-            left += 1
-        while array[right] > pivot:
-            right += 1
-        if left <= right:
-            swap(array, left, right)
-            left += 1
-            right -= 1
-    return left
+    if len(array) <= 1:
+        return array
+    pivot = random.choice(array)
+    smaller = [i for i in array if i < pivot]
+    larger = [i for i in array if i > pivot]
+    return quick_sort(smaller) + [i for i in array if i == pivot] + quick_sort(larger)
