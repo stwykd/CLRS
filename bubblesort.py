d3b92dac2c3acf5bcecb9bf82745c80d9ffe4575
stwykd <5012027+stwykd@users.noreply.github.com>
2020-05-09 08:24:32 +0100

    Use pythonic swap

--- bubblesort.py
+++ bubblesort.py
@@ -1,16 +1,10 @@
-def bubble_sort(array):
+def bubble_sort(arr):
     is_sorted = False
-    last_unsorted = len(array) - 1
+    last_unsorted = len(arr) - 1
     while not is_sorted:
         is_sorted = True
         for i in range(0, last_unsorted):
-            if array[i] > array[i+1]:
-                swap(array, i, i+1)
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
                 is_sorted = False
         last_unsorted -= 1
-
-
-def swap(array, i, j):
-    tmp = array[i]
-    array[i] = array[j]
-    array[j] = tmp
