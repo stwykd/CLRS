d91b60ea51e880d690cc535ecc44a88675ae3d48
stwykd <5012027+stwykd@users.noreply.github.com>
2020-05-22 19:07:05 +0100

    Add factors()

--- numerics.py
+++ numerics.py
@@ -76,6 +76,12 @@ assert filter(is_prime, range(1000)) == sieve(1000)
 
 
 
+def factors(n):    
+    return set(reduce(list.__add__, 
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+assert factors(1000) == set([1, 2, 4, 5, 8, 10, 20, 25, 40, 50, 100, 125, 200, 250, 500, 1000])
+
+
 def split_matrix(arr):
     h, w = arr.shape
     nrows, ncols = h/2, w/2
