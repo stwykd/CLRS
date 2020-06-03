1e758b988b429d961d55fb35b3c3a39656baff65
stwykd <5012027+stwykd@users.noreply.github.com>
2020-06-03 09:37:59 +0100

    Add tests in queue.py

--- queue.py
+++ queue.py
@@ -1,7 +1,7 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self, value):
         self.next = None
-        self.data = data
+        self.value = value
 
 
 class Queue:
@@ -10,28 +10,52 @@ class Queue:
         self.tail = None
 
     def is_empty(self):
-        return self.head is None
+        return not self.head
 
     def peek(self):
-        if self.head is None:
-            return -1
-        else:
-            return self.head.data
+        if self.head:
+            return self.head.value
 
-    def add(self, data):
-        node = Node(data)
-        if self.tail is not None:
+    def add(self, value):
+        node = Node(value)
+        if self.tail:
             self.tail.next = node
         self.tail = node
 
-        if self.head is None:
+        if not self.head:
             self.head = node
 
     def remove(self):
-        data = self.head.data
+        value = self.head.value
         self.head = self.head.next
 
-        if self.head is None:
+        if not self.head:
             self.tail = None
 
-        return data
+        return value
+
+    def __iter__(self):
+        current = self.head
+        while current:
+            yield current.value
+            current = current.next
+
+    def __str__(self):
+        return ' '.join(map(str, list(self)))
+
+
+q = Queue()
+q.add(0)
+q.add(1)
+q.add(2)
+q.add(3)
+assert list(q) == list(range(4))
+
+assert q.remove() == 0
+assert q.remove() == 1
+assert q.peek() == 2
+assert list(q) == [2, 3]
+
+q.remove()
+q.remove()
+assert q.is_empty()
