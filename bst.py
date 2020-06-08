b78fa252d24f8a4c931205bb442c73bac4353733
stwykd <5012027+stwykd@users.noreply.github.com>
2020-06-08 13:21:25 +0100

    Make code readable and add tests

--- bst.py
+++ bst.py
@@ -6,58 +6,59 @@ class Node(object):
         self.right = None
 
     def find(self, k):
+        # Returns a reference to the node with key `k`
         if k == self.key:
             return self
         elif k < self.key:
-            if self.left is None:
+            if not self.left:
                 return None
             else:
                 return self.left.find(k)
         else:
-            if self.right is None:
+            if not self.right:
                 return None
             else:
                 return self.right.find(k)
 
     def find_min(self):
         current = self
-        while current.left is not None:
+        while current.left:
             current = current.left
         return current
 
     def next_larger(self):
-        if self.right is not None:
+        if self.right:
             return self.right.find_min()
         current = self
-        while current.parent is not None and current is current.parent.right:
+        while current.parent and current is current.parent.right:
             current = current.parent
         return current.parent
 
     def insert(self, node):
-        if node is None:
+        if not node:
             return
         if node.key < self.key:
-            if self.left is None:
+            if not self.left:
                 node.parent = self
                 self.left = node
             else:
                 self.left.insert(node)
         else:
-            if self.right is None:
+            if not self.right:
                 node.parent = self
                 self.right = node
             else:
                 self.right.insert(node)
 
     def delete(self):
-        if self.left is None or self.right is None:
+        if not self.left or not self.right:
             if self is self.parent.left:
                 self.parent.left = self.left or self.right
-                if self.parent.left is not None:
+                if self.parent.left:
                     self.parent.left.parent = self.parent
             else:
                 self.parent.right = self.left or self.right
-                if self.parent.right is not None:
+                if self.parent.right:
                     self.parent.right.parent = self.parent
             return self
         else:
@@ -81,7 +82,7 @@ class BST(object):
 
     def insert(self, k):
         node = Node(None, k)
-        if self.root is None:
+        if not self.root:
             self.root = node
         else:
             self.root.insert(node)
@@ -93,7 +94,7 @@ class BST(object):
 
     def delete(self, k):
         node = self.find(k)
-        if node is None:
+        if not node:
             return None
         if node is self.root:
             temp = Node(None, 0)
@@ -101,7 +102,7 @@ class BST(object):
             self.root.parent = temp
             deleted = self.root.delete()
             self.root = temp.left
-            if self.root is not None:
+            if self.root:
                 self.root.parent = None
             return deleted
         else:
@@ -115,26 +116,34 @@ class BST(object):
         node = self.find(k)
         return node and node.next_larger()
 
-    def inorder_tree_walk(self, x):
-        if x:
-            self.inorder_tree_walk(x.left)
-            print x
-            self.inorder_tree_walk(x.right)
+    def inorder_tree_walk(self):
+        def _inorder(x):
+            if x:
+                _inorder(x.left)
+                nodes.append(x.key)
+                _inorder(x.right)
+        nodes = []
+        _inorder(self.root)
+        return nodes
 
 tree = BST()
-tree.insert_values([7,5,9,1,6,10,13,-3,-7,-1,0,-12])
-print tree.inorder_tree_walk(tree.root), tree.delete(-3)
-tree.inorder_tree_walk(tree.root)
-print
+l = [7,5,9,1,6,10,13,-3,-7,-1,0,-12]
+tree.insert_values(l)
+assert tree.inorder_tree_walk() == sorted(l)
 
-tree.delete_values([-12,5,0])
-tree.inorder_tree_walk(tree.root)
-print
+tree.delete(-3); l.remove(-3)
+assert tree.inorder_tree_walk() == sorted(l)
 
-print tree.find_min()
-tree.delete(-7)
-print tree.find_min()
+to_delete = [-12,5,0]
+tree.delete_values(to_delete)
+for x in to_delete:
+    l.remove(x)
+assert tree.inorder_tree_walk() == sorted(l)
 
-assert tree.find(10)
-assert tree.find(1)
-assert tree.find(3) is None
+assert tree.find_min().key == min(l)
+tree.delete(tree.find_min().key); l.remove(min(l))
+assert tree.find_min().key == min(l)
+
+to_find = [10,1,3]
+for x in to_find:
+    assert bool(tree.find(x)) == (x in l)
