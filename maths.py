71fe45bfe6486d8b6e68e7e7eeefa54984c67052
stwykd <5012027+stwykd@users.noreply.github.com>
2020-06-14 10:23:11 +0100

    Slighly change factors(n)

--- maths.py
+++ maths.py
@@ -79,9 +79,8 @@ assert filter(is_prime, range(1000)) == sieve(1000)
 
 
 
-def factors(n):
-    return set(reduce(list.__add__,
-                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+def factors(n): return set(reduce(list.__add__, \
+    ([i, n//i] for i in range(1, int(n**.5)+1) if n%i is 0)))
 
 assert factors(1000) == set([1, 2, 4, 5, 8, 10, 20, 25, 40, 50, 100, 125, 200, 250, 500, 1000])
 
